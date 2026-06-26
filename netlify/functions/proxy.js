e97081ee3ad948a711f1fd7ef34fbd304bb41a92
upco-production <admin.upco@gmail.com>
2026-06-26 20:57:05 +0500

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,32 +1,44 @@
 exports.handler = async function(event) {
   const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxeLyxLK1I9wc022j5JU6pjrd9G360lC9Sd1jCWm5Plkh0qy2u99jO8b8D-RyjgRkMORw/exec';
-  
+
+  const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+  };
+
   if (event.httpMethod === 'OPTIONS') {
-    return {
-      statusCode: 200,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-      },
-      body: ''
-    };
+    return { statusCode: 200, headers, body: '' };
   }
 
-  const qs = event.rawQuery ? '?' + event.rawQuery : '';
-  const response = await fetch(SCRIPT_URL + qs, {
-    method: event.httpMethod,
-    headers: { 'Content-Type': 'text/plain;charset=utf-8' },
-    body: event.body || undefined
-  });
+  try {
+    const isPost = event.httpMethod === 'POST';
+    const qs = event.rawQuery ? '?' + event.rawQuery : '';
+    
+    const fetchOptions = {
+      method: isPost ? 'POST' : 'GET',
+      headers: { 'Content-Type': 'text/plain;charset=utf-8' }
+    };
+    
+    if (isPost && event.body) {
+      fetchOptions.body = event.isBase64Encoded 
+        ? Buffer.from(event.body, 'base64').toString('utf8')
+        : event.body;
+    }
 
-  const data = await response.text();
-  return {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-      'Content-Type': 'application/json'
-    },
-    body: data
-  };
+    const response = await fetch(SCRIPT_URL + qs, fetchOptions);
+    const data = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: { ...headers, 'Content-Type': 'application/json' },
+      body: data
+    };
+  } catch(err) {
+    return {
+      statusCode: 500,
+      headers,
+      body: JSON.stringify({ ok: false, error: err.message })
+    };
+  }
 };
